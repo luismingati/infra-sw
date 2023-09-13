142405d699719cc461147be233239130936f0d50
Luis <luismingati@gmail.com>
2023-09-13 16:25:16 -0300

    funciona agora so cliar a fila uhu

--- main.c
+++ main.c
@@ -4,51 +4,56 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define MAX_LINE 80
-
-// - Execução interativa, saída com exit e style sequential - 10%
-// •Exemplo: myLogin seq> /bin/ls; /bin/ps
-
 int main(void) {
-char *args[MAX_LINE/2 + 1]; /* command line arguments */
-int should_run = 1; 
+  int should_run = 1;
 
   while (should_run) {
     printf("locm >");
     fflush(stdout);
-    char *command = NULL;
-    char *temp[10];
-    size_t n = 0;
-    ssize_t result = getline(&command, &n, stdin);
-    command[strcspn(command, "\n")] = 0;
 
-    if(strcmp(command, "exit") == 0) {
-      should_run = 0;
+    char *buffer = NULL;
+    size_t n = 0;
+    ssize_t result = getline(&buffer, &n, stdin);
+
+   if (result == -1) {
+      free(buffer);
+      continue;
     }
 
-    int i = 0;
-
-    do {
-      temp[0] = strtok(command, " ");
-      while (temp[++i] != NULL) {
-          temp[i] = strtok(NULL," ");
-      }
-    } while(strcmp(temp[0], "quit"));
-
-    free(command);
+    int bufferLength = strlen(buffer);
+    char *args[bufferLength / 2 + 1];
     
-    pid_t pid;
-    pid = fork();
+    buffer[strcspn(buffer, "\n")] = 0;
 
+    char *token = strtok(buffer, " ");
+    int index = 0;
+    while (token != NULL) {
+      args[index] = token;
+      token = strtok(NULL, " ");
+      index++;
+    }
+    args[index] = NULL;
+
+    if (args[0] && strcmp(args[0], "exit") == 0) {
+      free(buffer);
+      break;
+    }
+
+    pid_t pid = fork();
     if (pid < 0) {
       fprintf(stderr, "Fork Failed");
+      free(buffer);
       return 1;
     } else if (pid == 0) {
-      execvp(temp[0], temp);
+      execvp(args[0], args);
+      perror("locm >");  
+      exit(EXIT_FAILURE);
     } else {
       wait(NULL);
     }
     
+    free(buffer);
   }
+
   return 0;
 }
